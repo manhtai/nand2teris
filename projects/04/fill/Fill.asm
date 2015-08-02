9f13536fd2bed004122256dfdd9a4b46e8c1f441
manhtai <vo@manhtai.com>
2015-08-02 10:21:29 +0700

    Done week 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,74 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+
+(LOOP)
+    // arr = 16384 (base address of the Hack screen)
+    @SCREEN
+    D=A
+    @arr
+    M=D
+
+    // n=8192
+    @8192
+    D=A
+    @n
+    M=D
+
+    // initialize i = 0
+    @i
+    M=0
+    // check keyboard
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ
+    @BLACK
+    D;JNE
+
+(BLACK)
+    // if (i==n) go to LOOP
+    @i
+    D=M
+    @n
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // RAM[arr+i] = -1
+    @arr
+    D=M
+    @i
+    A=D+M
+    M=-1
+
+    // i++
+    @i
+    M=M+1
+
+    @BLACK
+    0;JMP
+
+(WHITE)
+    // if (i==n) go to LOOP
+    @i
+    D=M
+    @n
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // RAM[arr+i] = -1
+    @arr
+    D=M
+    @i
+    A=D+M
+    M=0
+
+    // i++
+    @i
+    M=M+1
+
+    @WHITE
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// i = 1
+// mul = 0;
+// while i <= R0:
+//     mul += R1
+//     i += 1
+
+    @i
+    M=0
+    @mul
+    M=0
+(LOOP)
+    @i          // D=i
+    D=M
+    @R0
+    D=D-M       // D=i-R0
+    @SAVE
+    D;JEQ       // If i-R0 > 0 go to SAVE
+    @R1
+    D=M         // D=R1
+    @mul
+    M=D+M       // mul = mul + R1
+    @i
+    M=M+1       // i=i+1
+    @LOOP
+    0;JMP       // Go to LOOP
+(SAVE)
+    @mul
+    D=M
+    @R2
+    M=D
+(END)
+    @END        // Infinite loop
+    0;JMP
